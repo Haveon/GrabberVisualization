df014cec8d8433c3b35883314263bcd4bd5da5ca
Victor <victor@Nexus>
2016-05-20 15:25:55 -0400

    Wrapped code in a class

--- Viz.py
+++ Viz.py
@@ -33,52 +33,104 @@ def frameTransform(quat, trans):
 
 #-------------------------------------------------------------
 
-topFile = LaptopFile('59435648-0-1000.txt')
-labFile = LabViewFile('59435648-0-1000-2016-05-15-at-23-36-37 .txt')
+class trialData:
+    def __init__(self, laptopFname, labViewFname):
+        topFile = LaptopFile(laptopFname)    #'59435648-0-1000.txt')
+        labFile = LabViewFile(labViewFname)  #'59435648-0-1000-2016-05-15-at-23-36-37 .txt')
 
-start = topFile.data[0].startTime
-end   = topFile.data[-1].startTime
-diff  = topFile.clockDifference
+        self.batchNumber     = topFile.batchNumber
+        self.objectNumber    = topFile.objectNumber
+        self.trialNumber     = topFile.trialNumber
+        self.clockDifference = topFile.clockDifference
+        self.servoStartPos   = topFile.servoStartPos
+        self.topData         = topFile.data
+        self.endGoal         = topFile.endGoal
+        self.endServoPos     = topFile.endServoPos
 
-labData = [i for i in labFile.data if end >= i.timeStamp-diff >= start]
+        self.tool1   = labFile.tool1
+        self.tool2   = labFile.tool2
+        self.labData = labFile.data
 
-x1,y1,z1 = [_.vec1[0] for _ in labData], [_.vec1[1] for _ in labData], [_.vec1[2] for _ in labData]
-x2,y2,z2 = [_.vec2[0] for _ in labData], [_.vec2[1] for _ in labData], [_.vec2[2] for _ in labData]
-x_min, x_max = min(x1+x2), max(x1+x2)
-y_min, y_max = min(y1+y2), max(y1+y2)
-z_min, z_max = min(z1+z2), max(z1+z2)
+        self._synchronize()
 
-for i in range(len(topFile.data)):
-    fig = plt.figure(figsize=(16,9), dpi=100)
-    ax = fig.add_subplot(121)
-    servoData = topFile.data[i].servoRead
-    index = np.arange(4)
-    bar_width = 0.35
+    def _synchronize(self):
+        """
+        This changes self.labData in place!!
+        """
+        start = self.topData[0].startTime
+        end   = self.topData[-1].startTime
+        diff  = self.clockDifference
 
-    rects = ax.bar(index, servoData, bar_width)
-    plt.xticks(index+0.5*bar_width, ('M1', 'M2', 'M3', 'M4'))
-    plt.ylim([14000,18000])
-    plt.title(str(topFile.data[i].startTime))
+        topTimeStamps = [_.startTime for _ in self.topData]        
+        labTimeStamps = []
+        for line in self.labData:
+            line.timeStamp -= diff
+            labTimeStamps.append(line.timeStamp)
 
-    vec1 = labData[i].vec1
-    qua1 = labData[i].qua1
-    x1,y1,z1 =  frameTransform(qua1, vec1)
+        indexes = []
+        for laptopTime in topTimeStamps:
+            tmp = [abs(labViewtime-laptopTime) for labViewtime in labTimeStamps]
+            indexes.append(np.argmin(tmp))
+            
+        self.labData = [self.labData[i] for i in indexes]
+        return
 
-    ax = fig.add_subplot(122, projection='3d')
-    ax.plot([x1[0],vec1[0]],[x1[1],vec1[1]],[x1[2],vec1[2]])
-    ax.plot([y1[0],vec1[0]],[y1[1],vec1[1]],[y1[2],vec1[2]])
-    ax.plot([z1[0],vec1[0]],[z1[1],vec1[1]],[z1[2],vec1[2]])
+    def _getAxisMeasurements(self):
 
-    vec2 = labData[i].vec2
-    qua2 = labData[i].qua2
-    x2,y2,z2 =  frameTransform(qua2, vec2)
+        x1 = [_.vec1[0] for _ in self.labData]
+        y1 = [_.vec1[1] for _ in self.labData]
+        z1 = [_.vec1[2] for _ in self.labData]
+        
+        x2 = [_.vec2[0] for _ in self.labData]
+        y2 = [_.vec2[1] for _ in self.labData]
+        z2 = [_.vec2[2] for _ in self.labData]
 
-    ax.plot([x2[0],vec1[0]],[x2[1],vec1[1]],[x2[2],vec1[2]])
-    ax.plot([y2[0],vec1[0]],[y2[1],vec1[1]],[y2[2],vec1[2]])
-    ax.plot([z2[0],vec1[0]],[z2[1],vec1[1]],[z2[2],vec1[2]])
+        xs,ys,zs = [_ for _ in x1+x2 if _ !=0.0], [_ for _ in y1+y2 if _ !=0.0], [_ for _ in z1+z2 if _ !=0.0]
+        x_min, x_max = min(xs), max(xs)
+        y_min, y_max = min(ys), max(ys)
+        z_min, z_max = min(zs), max(zs)
 
-    plt.title(str(labData[i].timeStamp-diff))
-    ax.set_xlim(x_min,x_max)
-    ax.set_ylim(y_min,y_max)
-    ax.set_zlim(z_min,z_max)
-    plt.savefig(str(i)+'.png')
+        return ((x_min,x_max),(y_min,y_max),(z_min,z_max))
+
+
+    def plotData(self, fname):
+        for i in range(len(self.labData)):
+            fig = plt.figure(figsize=(16,9), dpi=100)
+            ax = fig.add_subplot(121)
+            servoData = self.topData[i].servoRead
+            index = np.arange(4)
+            bar_width = 0.35
+
+            rects = ax.bar(index, servoData, bar_width)
+            plt.xticks(index+0.5*bar_width, ('M1', 'M2', 'M3', 'M4'))
+            plt.ylim([14000,18000])
+            plt.title(str(self.topData[i].startTime))
+
+            vec1 = self.labData[i].vec1
+            qua1 = self.labData[i].qua1
+            x1,y1,z1 =  frameTransform(qua1, vec1)
+
+            ax = fig.add_subplot(122, projection='3d')
+            ax.plot([x1[0],vec1[0]],[x1[1],vec1[1]],[x1[2],vec1[2]])
+            ax.plot([y1[0],vec1[0]],[y1[1],vec1[1]],[y1[2],vec1[2]])
+            ax.plot([z1[0],vec1[0]],[z1[1],vec1[1]],[z1[2],vec1[2]])
+
+            vec2 = self.labData[i].vec2
+            qua2 = self.labData[i].qua2
+            x2,y2,z2 =  frameTransform(qua2, vec2)
+
+            ax.plot([x2[0],vec2[0]],[x2[1],vec2[1]],[x2[2],vec2[2]])
+            ax.plot([y2[0],vec2[0]],[y2[1],vec2[1]],[y2[2],vec2[2]])
+            ax.plot([z2[0],vec2[0]],[z2[1],vec2[1]],[z2[2],vec2[2]])
+
+            plt.title(str(self.labData[i].timeStamp))
+            xBound, yBound, zBound = self._getAxisMeasurements()
+            ax.set_xlim(*xBound)
+            ax.set_ylim(*yBound)
+            ax.set_zlim(*zBound)
+            plt.savefig(fname+str(i)+'.png')
+            plt.close()
+
+if __name__ == '__main__':
+    d = trialData('59435648-0-1000.txt', '59435648-0-1000-2016-05-15-at-23-36-37 .txt')
+    d.plotData('')
